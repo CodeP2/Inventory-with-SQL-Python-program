c66e46f6f619f766cd54b8eb196e8e9f436bb104
CodeP2 <76018009+CodeP2@users.noreply.github.com>
2024-08-01 15:12:01 +0200

    needs work - handling duplicates

--- Inventory_script/import_csv.py
+++ Inventory_script/import_csv.py
@@ -1,18 +1,32 @@
 import menus
 import error_messages
 import pandas as pd
+import sys
+import os
+from Inventory_db import engine
 
 
-def add_product():
-    pass
+def path_to_file(directories, file_name):
+    script_file = os.path.dirname(__file__)
+    path_to_file = os.path.join(script_file, directories, file_name)
+    return os.path.normpath(path_to_file)
 
 
-def edit_product():
-    pass
+def read_csv(path):
+    return pd.read_csv(path)
 
 
-def delete_product():
-    pass
+def clean_with_pandas_inventory(df):
+    df["date_updated"] = pd.to_datetime(df["date_updated"], format='%m/%d/%Y')
+    df["product_price"] = pd.to_numeric(df["product_price"].str.replace("$", ""))
+    df["product_quantity"] = pd.to_numeric(df["product_quantity"])
+    return df
+
+
+def import_to_database(df):
+    with engine.begin() as connection:
+        df.duplicated(keep=False)
+        df.to_sql(name="Products", con=connection, if_exists="replace")
 
 
 def menu():
@@ -32,3 +46,21 @@ def menu():
                 print(f"Incorrect Choice: {option}")
         except ValueError:
             error_messages.menu_choice_error("1, 2, 3, 4, 5")
+
+
+if __name__ == "__main__":
+    file_name = sys.argv[2]
+    directory = sys.argv[1]
+
+    my_csv_path = path_to_file(directory, file_name)
+
+    print(my_csv_path)
+
+    df = read_csv(my_csv_path)
+
+    clean_data = clean_with_pandas_inventory(df)
+    print(clean_data)
+    clean_data = clean_data.drop_duplicates(keep="last", subset="product_name")
+    import_to_database(clean_data)
+
+
